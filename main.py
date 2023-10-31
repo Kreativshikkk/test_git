35c3bebf207e9c6bbab7b9e60a1109d5a6a1a8d0
Kreativshikkk <nikiniks2005@gmail.com>
2023-10-31 23:33:07 +0200

    parser commit

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-import parser, os, sys
+import os
+import parser
+import sys
 
-args = parser.get()
-if args.command == '1':
-    print(sys.version)
-elif args.command == '2':
-    os.mkdir('./'.join(args.name))
-elif args.command == '3':
-    print(os.listdir('..'))
+
+def first_command():
+    return sys.version
+
+
+def second_command(directory_name):
+    os.mkdir(f'./{directory_name}')
+
+
+def third_command():
+    return os.listdir('..')
+
+
+def main():
+    args = parser.get()
+    if args.command == '1':
+        print(first_command())
+    elif args.command == '2':
+        second_command(args.name)
+    elif args.command == '3':
+        print(third_command())
+
+
+if __name__ == '__main__':
+    main()
